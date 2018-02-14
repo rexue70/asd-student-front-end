b08efe932047d49b3e03598f5377a76f3b6254fd
Minghao23 <hu_minghao@users.noreply.github.com>
2018-02-13 17:47:27 -0800

    Add NavBar component

--- student-front-end/src/App.js
+++ student-front-end/src/App.js
@@ -1,17 +1,7 @@
 import React, {Component} from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom';
-import Home from './components/home/Home';
 import Header from './components/Header';
-import Profile from './components/profile/Profile';
-import Academic from './components/academic/Academic';
-import Coop from './components/coop/Coop';
-import Resources from './components/resources/Resources';
-import Header_Yudong from './components/Header_Yudong';
 import Footer from "./components/Footer";
+import NavigationBar from "./components/NavigationBar";
 
 
 class App extends Component {
@@ -19,23 +9,7 @@ class App extends Component {
         return (
             <div>
                 <Header/>
-                <Router>
-                    <div id="main-nav">
-                        <ul className="nav-row">
-                            <li><Link to="/profile" className="nav-text">Profile</Link></li>
-                            <li><Link to="/academic" className="nav-text">Academic</Link></li>
-                            <li><Link to="/resources" className="nav-text">Resources</Link></li>
-                            <li><Link to="/coop" className="nav-text">COOP</Link></li>
-                            <li><Link to="/" className="nav-text">Home</Link></li>
-                        </ul>
-                        <Route exact path="/" component={Home}/>
-                        <Route exact path="/profile" component={Profile}/>
-                        <Route path="/academic" component={Academic}/>
-                        <Route path="/coop" component={Coop}/>
-                        <Route path="/resources" component={Resources}/>
-
-                    </div>
-                </Router>
+                <NavigationBar/>
                 <Footer/>
             </div>
         );
---
+++ student-front-end/src/components/NavigationBar.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+import Profile from "./profile/Profile";
+import Academic from "./academic/Academic";
+import Home from "./home/Home";
+import Resources from "./resources/Resources";
+import Coop from "./coop/Coop";
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            <Router>
+                <div id="main-nav">
+                    <ul className="nav-row">
+                        <li><Link to="/profile" className="nav-text">Profile</Link></li>
+                        <li><Link to="/academic" className="nav-text">Academic</Link></li>
+                        <li><Link to="/resources" className="nav-text">Resources</Link></li>
+                        <li><Link to="/coop" className="nav-text">COOP</Link></li>
+                        <li><Link to="/" className="nav-text">Home</Link></li>
+                    </ul>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/profile" component={Profile}/>
+                    <Route path="/academic" component={Academic}/>
+                    <Route path="/coop" component={Coop}/>
+                    <Route path="/resources" component={Resources}/>
+
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default NavigationBar
