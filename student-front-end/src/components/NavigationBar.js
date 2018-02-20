ee6faf3180b61488751785eb48b755ba87b9d576
Eddie <wangyudong53138@gmail.com>
2018-02-20 15:59:14 -0800

    add profile0220

--- student-front-end/src/components/NavigationBar.js
+++ student-front-end/src/components/NavigationBar.js
@@ -8,23 +8,68 @@ import Profile from "./profile/Profile";
 import Academic from "./academic/Academic";
 import Home from "./home/Home";
 import Resources from "./resources/Resources";
+import {Navbar, NavItem, Nav, NavDropdown, MenuItem} from 'react-bootstrap';
+
 
 class NavigationBar extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeKey: 1,
+        };
+    }
+
+
+    handleSelect(selectedKey) {
+        console.log("this is", selectedKey);
+        this.setState({activeKey: selectedKey});
+    }
+
     render() {
         return (
             <Router>
+
+
                 <div id="main-nav">
-                    <ul className="nav-row">
-                        <li><Link to="/profile" className="nav-text">Profile</Link></li>
-                        <li><Link to="/academic" className="nav-text">Academic</Link></li>
-                        <li><Link to="/resources" className="nav-text">Resources</Link></li>
-                        <li><Link to="/" className="nav-text">Home</Link></li>
-                    </ul>
+                    <Navbar collapseOnSelect className="navbar-trans">
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                <a href="/">Northeastern University</a>
+                            </Navbar.Brand>
+                            <Navbar.Toggle />
+                        </Navbar.Header>
+                        <Navbar.Collapse>
+                            <Nav activeKey={this.state.activeKey} onSelect={this.handleSelect}>
+                                <NavItem eventKey={1} href="/">
+                                    Home
+                                </NavItem>
+                                <NavItem eventKey={2} href="/profile">
+                                    Profile
+                                </NavItem>
+                                <NavItem eventKey={3} href="/academic">
+                                    Academic
+                                </NavItem>
+                                <NavItem eventKey={4} href="/resources">
+                                    Resources
+                                </NavItem>
+
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+
+
+                    {/*<ul className="nav-row">*/}
+                    {/*<li><Link to="/profile" className="nav-text">Profile</Link></li>*/}
+                    {/*<li><Link to="/academic" className="nav-text">Academic</Link></li>*/}
+                    {/*<li><Link to="/resources" className="nav-text">Resources</Link></li>*/}
+                    {/*<li><Link to="/" className="nav-text">Home</Link></li>*/}
+                    {/*</ul>*/}
                     <Route exact path="/" component={Home}/>
                     <Route exact path="/profile" component={Profile}/>
                     <Route path="/academic" component={Academic}/>
                     <Route path="/resources" component={Resources}/>
-
                 </div>
             </Router>
         )
--- student-front-end/src/components/profile/Profile.js
+++ student-front-end/src/components/profile/Profile.js
@@ -1,23 +1,120 @@
 import React, {Component} from 'react';
-import profile_image from '../../images/profile.jpg';
+import styled from 'styled-components'
+import ProfileSelect from "./ProfileSelect";
+import ProfileInput from "../profile/ProfileInput";
+import ProfileCard from "../profile/ProfileCard"
+
+
+const options = {
+    Gender: {
+        text: 'Select your gender:',
+        data: ['Male', 'Female'],
+    },
+    Age: {
+        text: 'Select your age',
+        data: [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+    },
+    Email: {
+        text: 'Input your email',
+    },
+    Address: {
+        text: 'Input your address',
+    },
+    Phone: {
+        text: 'Input your Phone',
+    },
+    Campus: {
+        text: 'Select your campus',
+        data: ['Boston', 'Seattle', 'Silicon Valley'],
+    },
+    StartTerm: {
+        text: 'Select your start term',
+        data: ['Spring 2016', 'Fall 2016', 'Spring 2017', 'Fall 2017', 'Spring 2018'],
+    },
+    ExpectedGraduation: {
+        text: 'Select your expected Graduation',
+        data: ['June 2018', 'Aug 2018', 'Dec 2018'],
+    },
+    Major: {
+        text: 'Select your major',
+        data: ['Computer Science Align', 'Information System', 'Product Management'],
+    },
+    Degree: {
+        text: 'Select your degree',
+        data: ['Bachelor', 'Master', 'Phd'],
+    },
+    Enrollment: {
+        text: 'Enrollment Student',
+        data: ['Yes (active student)', 'No'],
+    },
+    Citizenship: {
+        text: 'Select your citizenship',
+        data: ['America', 'Chinese', 'Indian', 'Canadian'],
+    }
+
+};
+
 
 class Profile extends Component {
+
 
     render() {
         return (
             <div>
+                {/*<div style={{width: '100%'}}>*/}
+                    <ProfileCard />
+                {/*</div>*/}
 
-                Profile assigned to Yudong
-                {/*<img height="1942" width="max-width:100%;" src={profile_image} />*/}
+                <div style={{display: 'table', width: '100%'}}>
+                    <div style={{display: 'table-cell'}}>
+                        <FormCss>
+                            <ProfileSelect options={options.Gender}/>
+                            <ProfileSelect options={options.Age}/>
+                            <ProfileInput options={options.Email}/>
+                            {/*<ProfileInput options={options.Address}/>*/}
+                            {/*<ProfileInput options={options.Phone}/>*/}
+                            <ProfileSelect options={options.Campus}/>
+                        </FormCss>
+                    </div>
 
+                    <div style={{display: 'table-cell'}}>
+                        <FormCss>
+                            <ProfileSelect options={options.StartTerm}/>
+                            <ProfileSelect options={options.ExpectedGraduation}/>
+                            <ProfileSelect options={options.Major}/>
+                            <ProfileSelect options={options.Degree}/>
+                            <ProfileSelect options={options.Enrollment}/>
+                            {/*<ProfileSelect options={options.Citizenship}/>*/}
+                        </FormCss>
+                    </div>
+                </div>
 
+                <UpdateButton>Update</UpdateButton>
 
             </div>
-
-
         )
     }
 }
+
+
+const FormCss = styled.div`
+    margin: auto;
+    display: table-cell;
+    float:right;
+    margin-right: 25%;
+    `
+
+const UpdateButton = styled.button`
+    margin-right: 35%;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    float: right;
+    width:50%;
+    table-layout: fixed;
+    height:30px;
+    background: pink;
+    border-radius: 30px;
+    `
 
 
 export default Profile
