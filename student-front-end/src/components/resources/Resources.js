f1f0d5034349d46dbe1d95a36415117ecddcccad
Minghao23 <hu_minghao@users.noreply.github.com>
2018-02-27 09:06:43 -0800

    Add Resource page

--- student-front-end/src/components/resources/Resources.js
+++ student-front-end/src/components/resources/Resources.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ResourceContent from "./ResourceContent";
 
 
 class Resources extends Component {
@@ -6,7 +7,7 @@ class Resources extends Component {
     render() {
         return (
             <div>
-                Resources
+                <ResourceContent/>
             </div>
         )
     }
