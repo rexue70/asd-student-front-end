77f6b60e2172f350fa0347b5e886639810e45437
Xu Hang <438034612@qq.com>
2018-03-15 19:49:33 -0700

    add search component

---
+++ student-front-end/src/components/search/Search.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import Filter from "./Filter";
+import SearchResult from "./SearchResult";
+import { Grid, Row, Col, css } from 'react-bootstrap';
+import { ReactGridLayout } from '{universe:react-grid-layout}';
+
+class Search {
+
+    render() {
+        return (
+        var layout = [
+            {i: 'filter', x: 0, y: 0, w: 4, h: 30, static: true},
+            {i: 'searchResult', x: 1, y: 0, w: 8, h: 30, static: true}
+        ];
+        return (
+            <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+                <div key="filter"><Filter/></div>
+                <div key="searchResult"><SearchResult/></div>
+            </ReactGridLayout>
+        )
+    }
+}
+
+export default Search
